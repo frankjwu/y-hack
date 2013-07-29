b7298c35c8a84055a96aad5fda3fca251d33be32
frankjwu <me@frankjwu.com>
2013-07-29 01:52:02 -0700

    New stylesheets for different screen sizes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
 	setInterval("blinkCursor()", 0);
-	var screensize = $(document).width();
-	if (screensize < 1000)
-		$("#big-y").hide();
 });
 
 function blinkCursor() {
