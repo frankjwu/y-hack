cc847697af1d5b25518e6611b5ab7d5d93d27540
frankjwu <me@frankjwu.com>
2013-08-06 16:28:07 -0700

    switch to fadeout and fadein to fix certain browsers

--- js/scroll.js
+++ js/scroll.js
@@ -6,8 +6,8 @@ $(document).ready(function() {
 
 function scrollChanges() {
 	var scrolled = $(window).scrollTop();
-	if (scrolled > 3000)
-		$('#parallax-bg1').css('display', 'none');
+	if (scrolled > 2400)
+		$('#parallax-bg1').fadeOut(250);
 	else
-		$('#parallax-bg1').css('display', 'block');
+		$('#parallax-bg1').fadeIn(250);
 }
