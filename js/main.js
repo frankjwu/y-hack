2177e9535cee82f19a422470d6cf1597f5c5ec47
frankjwu <me@frankjwu.com>
2013-08-27 18:17:18 -0400

    Change bg image on hover

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
 	setInterval("blinkCursor()", 0);
+
+	var hover = true;
+	$('#link').hover(function() {
+		if (hover)
+			changeBG();
+	})
 });
 
 function blinkCursor() {
 	$('.cursor').fadeIn(600);
 	$('.cursor').fadeOut(400);
+}
+
+function changeBG() {
+	$('#bottom').css('background-image', 'url("/img/Yale3.jpg")');
+	$('#bottom h1').text("Welcome to Y-Hack!");
+	hover = false;
 }
