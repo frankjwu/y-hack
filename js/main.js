5ee4a60dc51a36dcc9a6fde1218a4766a6e62c84
frankjwu <me@frankjwu.com>
2013-09-20 16:05:28 -0400

    Remove changing background

--- js/main.js
+++ js/main.js
@@ -1,20 +1,8 @@
 $(document).ready(function() {
 	setInterval("blinkCursor()", 0);
-
-	var hover = true;
-	$('#link').hover(function() {
-		if (hover)
-			changeBG();
-	})
 });
 
 function blinkCursor() {
 	$('.cursor').fadeIn(600);
 	$('.cursor').fadeOut(400);
-}
-
-function changeBG() {
-	$('#bottom').css('background-image', 'url("/img/Yale3.jpg")');
-	$('#bottom h1').text("Welcome to Y-Hack!");
-	hover = false;
 }
